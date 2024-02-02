35d2c47dcf2092d02ff72dcb5d33c2c648c65a8b
dynosaur72 <61371403+dynosaur72@users.noreply.github.com>
2024-02-03 01:31:15 +0900

    T/L menu.js
    draft 0
    everything from 후식맨TV

--- js/game/menu.js
+++ js/game/menu.js
@@ -12,17 +12,17 @@ function Start(){
 
 	//////////////////////////////
 
-	N("<b>COMING OUT SIMULATOR</b>");
-	N("A half-true story about half-truths.");
-	N("Hey there, player. Glad to have you for the next 20 minutes, I guess.");
-	N("What would you like to do now?");
+	N("<b>커밍아웃 시뮬레이터 2014</b>");
+	N("반쪽짜리 진실에 대한 반쯤 진실인 게.");
+	N("안녕, 거기 플레이어 씨. 이 게임에 온 걸 환영해.");
+	N("이제 뭘 해볼래?");
 
 	Choose({
-		"Let's play this thing!": Play,
-		"Who are you? (About Me)": function(){
+		"플레이해보자!": Play,
+		"넌 누군데? (크레딧)": function(){
 			Credits("Who are you?");
 		},
-		"Hm, tell me more. (About This Game)": function(){
+		"흠, 더 알려줘. (게임에 대한 정보)": function(){
 			About("Hm, tell me more.");
 		}
 	});
@@ -44,9 +44,9 @@ function Play(message){
 
 	// Asked neither
 	if(!$.asked_about && !$.asked_credits){
-		N("Jumping right into it! Great!");
+		N("바로 시작하는구나! 좋아!");
 		N("No messing around with reading the About Me or the About This Game sections or--");
-		p("Shush.");
+		p("쉿.");
 		N("Fine, fine.");
 	}
 	// Asked both
@@ -56,17 +56,17 @@ function Play(message){
 		N("NO IDEA");
 	// Asked either
 	}else if($.asked_about || $.asked_credits){
-		N("Yes, let's!");
+		N("그래, 그러!");
 	}
 
-	N("Let's travel back five years ago, to 2010...");
-	p("That was FIVE years ago?!");
-	N("...to the evening that changed my life forever.");
+	N("4년 전, 2010년으로 돌아가보자...");
+	p("그게 4년 전이었다고?!");
+	N("...내 인생을 영원히 바꿔버린 그 저녁으로 말이.");
 
-	N("Tell me, dear player, how do you think this all ends?");
+	N("친애하는 플레이어 씨. 이 게임이 어떻게 끝날거라 생각해?");
 
 	Choose({
-		"With flowers and rainbows and gay unicorns?": function(message){
+		"꽃이랑 무지개랑 게이 유니콘으로?": function(message){
 			$.main_menu_convo_1 = 1;
 
 			p(message);
@@ -75,18 +75,18 @@ function Play(message){
 			N("No.");
 			Play_2();
 		},
-		"Apparently, with you redditing at Starbucks.": function(message){
+		"보니까 스타벅스에서 레딧질이나 하는걸로.": function(message){
 			$.main_menu_convo_1 = 2;
 
 			p(message);
-			N("Hey, I'm coding on this laptop. Turning my coming-of-age story into the game you're playing right now.");
-			p("Naw, you're probably procrastinating.");
-			N("Look who's talking.");
-			p("Touché, douché.");
-			N("Anyway...");
+			N("야, 나 코딩중이거든. 내 성장 스토리를 지금 네가 하고 있는 게임으로 만드는 중이라고.");
+			p("에이, 너 그냥 뺀질거리는 거잖아."); //너 그냥 할일을 미루고 있는 거겠지.
+			N("네가 할 말은 아닐텐데.");
+			p("...인정.");
+			N("어쨌든...");
 			Play_2();
 		},
-		"IT ALL ENDS IN BLOOD": function(message){
+		"<b>전부 피로 끝나지</b>": function(message){ // awk tl (IT ALL ENDS IN BLOOD)
 			$.main_menu_convo_1 = 3;
 
 			p(message);
@@ -107,13 +107,13 @@ function Play_2(){
 		p("Shush.");
 	}
 
-	N("This game includes dialogue that I, my parents, and my ex-boyfriend actually said.");
-	N("As well as all the things we could have, should have, and never would have said.");
-	N("It doesn't matter which is which.");
-	N("Not anymore.");
+	N("이 게임은 나, 내 부모님, 내 전 남친이 전부 실제로 했던 대화를 담고 있어.");
+	N("그리고 우리가 했었을 수도 있고, 했어야 했고, 절대 하지 않았을 말들도 담고 있지.");
+	N("어떤게 어느 쪽인지는 중요하지 않아.");
+	N("더이상은.");
 
 	Choose({
-		"How can I win a game with no right answers?": function(message){
+		"정답이 없는 게임을 어떻게 이기라는 거야?": function(message){
 			$.main_menu_convo_2 = 2;
 
 			p(message);
@@ -121,19 +121,19 @@ function Play_2(){
 			p(". . .");
 			Play_3();
 		},
-		"You're a bit of a downer, aren't you?": function(message){
+		"너 좀 우울한 놈이구나?": function(message){
 			$.main_menu_convo_2 = 1;
 
 			p(message);
-			N("LIFE is a bit of a downer.");
+			N("<b>인생은</b> 우울한거야.");
 			p("So that's a yes.");
 			Play_3();
 		},
-		"This 'true' game is full of lies?": function(message){
+		"이 '진실인' 게임이 거짓으로 가득 차 있다고?": function(message){
 			$.main_menu_convo_2 = 3;
 
 			p(message);
-			N("Even if the dialogue was 100% accurate, it'd still be 100% lies.");
+			N("대화를 100% 정확히 복기했다고 해도, 100% 거짓말로 돼있었을거야.");
 			p(". . .");
 			Play_3();
 		}
@@ -143,10 +143,10 @@ function Play_2(){
 
 function Play_3(){
 
-	N("You'll be playing as me, circa 2010.");
+	N("넌 2010년의 나로 플레이하게 될거.");
 	if(!$.asked_credits){
-		N("Because you skipped the About Me, my (not-yet-legal) name is Nicky Case. Just so you know.");
-		p("Shush.");
+		N("네가 크레딧을 씹어서 해주는 말인데, 내 (아직 서류상으로는 못 올린) 이름은 니키 케이스야.");
+		p("쉬잇.");
 	}
 
 	var whatISay;
@@ -252,31 +252,31 @@ function About(message){
 	SipCoffee(message);
 
 	if($.asked_credits){
-		N("I wanted to tell my story.");
+		N("내 이야기를 하고 싶었어.");
 	}else{
-		N("This game...");
-		N("...more like a conversation simulator, really...");
-		N("...is a very personal story.");
+		N("이 게임...");
+		N("...사실 대화 시물레이터에 가깝긴 하지...");
+		N("...이건 굉장히 개인적인 이야기.");
 	}
 	
-	p("Of course. You narcissist.");
-	N("Ha, of course.");
+	p("그러시겠지. 이 나르시시스트.");
+	N("하, 물론이.");
 
 	if($.asked_credits){
-		p("Actually no, a narcissist would use their real name.");
-		N("I told you, it IS my real na--");
-		p("Aight, aight. Weirdo.");
+		p("아니, 나르시시스트는 자기 진짜 이름을 쓰겠지.");
+		N("내 진짜 이름 맞다고 말했--");
+		p("알겠어, 알겠어. Weirdo.");
 	}
 
-	N("I made this game for the #Nar8 Game Jam. Gave me an excuse. And a deadline!");
-	p("You procrastinated until the last day to enter, didn't you.");
-	N("Yes.");
-	N("Also! This game is uncopyrighted. Dedicated to the public domain.");
-	N("I'm as open with my source code as I am with my sexuality.");
+	N("이 게임은 #Nar8 Game Jam에 내려고 만들었어. 만들 핑계가 생겼지. 마감기한도!");
+	p("완전 마지막 날까지 미루다 제출했구나.");
+	N("응.");
+	N("그리고! 이 게임에는 저작권이 없어. 퍼블릭 도메인이지.");
+	N("난 내 소스코드에 대해선 내 섹슈얼리티만큼 공개적이라.");
 
-	p("Ugh, that's a terrible pun.");
+	p("으, 설마 그거 드립?");
 	N("Howzabout a 'Fork Me' programming pun?");
-	p("noooooo.");
+	p("안돼애애애.");
 
 	if($.asked_credits){
 		Choose({
@@ -284,9 +284,9 @@ function About(message){
 		});
 	}else{
 		Choose({
-			"Bad puns aside, can we play now?": Play,
-			"So who ARE you? (About Me)": function(){
-				Credits("So who ARE you?");
+			"몹쓸 드립같은건 제쳐두고, 플레이하면 안될?": Play,
+			"그래서 대체 넌 누군데? (크레딧)": function(){
+				Credits("그래서 대체 넌 누군데?");
 			}
 		});
 	}
